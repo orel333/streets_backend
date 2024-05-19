79218874030a0661c12fa9972d866329f6591443
orel3 <orel333@mail.ru>
2024-05-20 02:21:51 +0300

    news api

--- streets_backend/blog/models.py
+++ streets_backend/blog/models.py
@@ -1,9 +1,16 @@
 from django.db import models
+
+from aboutus.models import Region
 from users.models import CustomUser
 
 
+POST_CHOICES = (
+    ('post', 'Пост'),
+    ('news', 'Новость')
+)
+
 class BlogPost(models.Model):
-    '''Модель для постов в блоге'''
+    '''Модель для постов в блоге.'''
     author = models.ForeignKey(
         CustomUser,
         on_delete=models.CASCADE,
@@ -24,7 +31,40 @@ class BlogPost(models.Model):
         'Дата обновления',
         auto_now=True
     )
+    image = models.ImageField(
+        'Картинка',
+        upload_to='blogs/images/',
+        null=False
+    )
+    type = models.CharField(
+        'Тип поста',
+        choices=POST_CHOICES,
+        default='post',
+        max_length=4
+    )
+    # TODO: обязательно при простановке новости
+    relevance_date = models.DateField(
+        'Крайняя дата актуальности новости'
+    )
+    # TODO: валидация что связь с регионом только если тип новость
+    region = models.ManyToManyField(Region, through='PostRegion')
 
     class Meta:
         verbose_name = 'Пост блога'
         verbose_name_plural = 'Посты блога'
+
+    def __str__(self):
+        return '"{}", {}. От {}'.format(
+            self.title,
+            self.author,
+            self.created_at
+        )
+
+
+class PostRegion(models.Model):
+    '''Модель связи поста с регионом.'''
+    news = models.ForeignKey(BlogPost, on_delete=models.CASCADE)
+    region = models.ForeignKey(Region, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '{}, {}'.format(self.news, self.region)
--- streets_backend/blog/serializers.py
+++ streets_backend/blog/serializers.py
@@ -1,11 +1,33 @@
 from rest_framework import serializers
 from blog.models import BlogPost
 
+from utils.fields import Base64ImageField
+
 
 class BlogPostSerializer(serializers.ModelSerializer):
     '''Сериализатор для постов в блоге'''
+    image = Base64ImageField(required=True, allow_null=False)
+    author = serializers.SlugRelatedField(
+        slug_field='username'
+    )
+    region = serializers.SlugRelatedField(
+        many=True,
+        slug_field='name'
+    )
+
     class Meta:
         model = BlogPost
         fields = [
-            'id', 'author', 'title', 'content', 'created_at', 'updated_at'
+            'id',
+            'author',
+            'title',
+            'content',
+            'created_at',
+            'updated_at',
+            'image',
+            'type',
+            'relevance_date',
+            'region'
         ]
+
+    #TODO валидация type, создание relevance_date
