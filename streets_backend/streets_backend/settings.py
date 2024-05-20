34c46a7d4b202ef15fc588e15c3f15967db7ae31
Pavel Okhrim <paokhrim@gmail.com>
2024-05-20 21:22:45 +0300

    Доработан docker-compose.yml. Добавлен волюм с базой PostgreSQL. Настройки PostgreSQL внесены в .env.example, settings.py.

--- streets_backend/streets_backend/settings.py
+++ streets_backend/streets_backend/settings.py
@@ -5,13 +5,9 @@ from datetime import timedelta
 from dotenv import load_dotenv
 from pathlib import Path
 
-<<<<<<< HEAD
 dotenv_path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'infra', '.env')
 load_dotenv(dotenv_path)
 
-=======
-load_dotenv()
->>>>>>> 47dd3a8 (Настройка workflows для деплоя на сервер.)
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 BASE_URL = os.getenv('BASE_URL')
@@ -22,11 +18,7 @@ DEBUG = bool(os.getenv('DEBUG', default=False))
 
 EMPTY_VALUE: str = '-пусто-'
 
-<<<<<<< HEAD
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
-=======
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
->>>>>>> 47dd3a8 (Настройка workflows для деплоя на сервер.)
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -36,12 +28,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-<<<<<<< HEAD
     'rest_framework.authtoken',
     'myauth.apps.MyauthConfig',
-=======
-    'rest_framework_simplejwt',
->>>>>>> 47dd3a8 (Настройка workflows для деплоя на сервер.)
     'contacts.apps.ContactsConfig',
     'blog.apps.BlogConfig',
     'users.apps.UsersConfig',
