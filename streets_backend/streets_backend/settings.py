de5f399fd97d6d86c32394dc2e325e44270f9e29
Pavel Okhrim <paokhrim@gmail.com>
2024-05-20 21:22:45 +0300

    Доработан docker-compose.yml. Добавлен волюм с базой PostgreSQL. Настройки PostgreSQL внесены в .env.example, settings.py.

--- streets_backend/streets_backend/settings.py
+++ streets_backend/streets_backend/settings.py
@@ -60,8 +60,12 @@ WSGI_APPLICATION = 'streets_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
