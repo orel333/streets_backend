9c8f1cb5bbf23eef7b8f14a5a4b05b3d7bb18667
Pavel Okhrim <paokhrim@gmail.com>
2024-05-22 13:16:56 +0300

    Тип БД изменен обратно на PostreSQL.

--- streets_backend/streets_backend/settings.py
+++ streets_backend/streets_backend/settings.py
@@ -71,8 +71,12 @@ WSGI_APPLICATION = 'streets_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
