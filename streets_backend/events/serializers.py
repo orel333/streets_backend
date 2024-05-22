39a8c60d9545663936304546931d2f011a29637d
Pavel Okhrim <paokhrim@gmail.com>
2024-05-22 13:14:02 +0300

    Добавлена модель мест проведения мероприятий и вьюсеты, сериализаторы, маршруты, админка. Workflows изменен на деплой на удаленный сервер только ветки develop.

--- streets_backend/events/serializers.py
+++ streets_backend/events/serializers.py
@@ -1,11 +1,27 @@
 from rest_framework import serializers
-from .models import Event
+from .models import Coordinates, EventLocation, Event
+
+
+class CoordinatesSerializer(serializers.ModelSerializer):
+    """Сериализатор для модели Coordinates"""
+    class Meta:
+        model = Coordinates
+        fields = ['latitude', 'longitude']
+
+
+class EventLocationSerializer(serializers.ModelSerializer):
+    """Сериализатор для модели EventLocation"""
+    coordinates = CoordinatesSerializer(read_only=True)
+    
+    class Meta:
+        model = EventLocation
+        fields = ['name', 'description', 'address', 'coordinates']
 
 
 class EventSerializer(serializers.ModelSerializer):
     """Сериализатор для модели Event"""
+    place = EventLocationSerializer(read_only=True)
+    
     class Meta:
         model = Event
-        fields = [
-            'id', 'name', 'time', 'date', 'place', 'description'
-        ]
+        fields = ['id', 'name', 'time', 'date', 'place', 'description']
