814a58097452a32a5e1c7cebb77bc2a0182f3a5e
Pavel Okhrim <paokhrim@gmail.com>
2024-05-24 23:08:29 +0300

    Проект переведен на постгрес.

--- streets_backend/settings.py
+++ streets_backend/settings.py
@@ -72,8 +72,12 @@ WSGI_APPLICATION = 'streets_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'default_db_name'),
+        'USER': os.getenv('POSTGRES_USER', 'default_user'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'default_password'),
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
