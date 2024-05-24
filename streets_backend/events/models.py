5e891864c47676a21684280ef90fa4566c598fef
Pavel Okhrim <paokhrim@gmail.com>
2024-05-24 08:47:42 +0300

    Приложение Event доработано с учетом требований фронтенда. Добавлены модели дисциплины и региона проведения мероприятий.

--- streets_backend/events/models.py
+++ streets_backend/events/models.py
@@ -1,8 +1,46 @@
 from django.db import models
 
 
+class Discipline(models.Model):
+    '''Модель дисциплины'''
+    name = models.CharField(
+        'Название дисциплины',
+        max_length=50,
+        unique=True
+    )
+
+    class Meta:
+        verbose_name = 'Дисциплина'
+        verbose_name_plural = 'Дисциплины'
+
+    def __str__(self):
+        return self.name
+
+
+class Region(models.Model):
+    '''Модель региона'''
+    name = models.CharField(
+        'Название региона',
+        max_length=255,
+        unique=True
+    )
+
+    class Meta:
+        verbose_name = 'Регион'
+        verbose_name_plural = 'Регионы'
+
+    def __str__(self):
+        return self.name
+
+
 class Event(models.Model):
-    """Модель мероприятия для интерактивного календаря"""
+    '''Модель мероприятия'''
+    EVENT_TYPES = (
+        ('competition', 'Соревнование'),
+        ('training', 'Тренировка'),
+        ('event', 'Мероприятие'),
+    )
+
     name = models.CharField(
         'Название мероприятия',
         max_length=255
@@ -19,6 +57,21 @@ class Event(models.Model):
     )
     description = models.TextField(
         'Описание мероприятия'
+    )
+    event_type = models.CharField(
+        'Тип события',
+        max_length=50,
+        choices=EVENT_TYPES
+    )
+    discipline = models.ForeignKey(
+        Discipline,
+        on_delete=models.CASCADE,
+        verbose_name='Дисциплина'
+    )
+    region = models.ForeignKey(
+        Region,
+        on_delete=models.CASCADE,
+        verbose_name='Регион проведения'
     )
 
     class Meta:
