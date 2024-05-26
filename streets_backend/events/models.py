90810a2a3c720be18be88ae6d34f2851c0e8645d
Pavel Okhrim <paokhrim@gmail.com>
2024-05-26 21:50:38 +0300

    Функционал добавления QR-кодов, содержащих ссылки на мероприятия.

--- streets_backend/events/models.py
+++ streets_backend/events/models.py
@@ -1,4 +1,28 @@
 from django.db import models
+from django.urls import reverse
+from django.http import HttpResponse
+from django.views import View
+from django.contrib.auth.mixins import UserPassesTestMixin
+from PIL import Image, ImageDraw
+from io import BytesIO
+from django.core.files import File
+import qrcode
+from decouple import Config
+
+config = Config(".env")
+
+
+class QRCode(models.Model):
+    '''Модель QR-кода'''
+    event = models.OneToOneField(
+        'Event',
+        on_delete=models.CASCADE,
+        related_name='qrcode'
+    )
+    image = models.ImageField(
+        upload_to='qrcodes',
+        blank=True
+    )
 
 
 class Event(models.Model):
@@ -56,12 +80,50 @@ class Event(models.Model):
         choices=DISCIPLINES
     )
 
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        self.create_qr_code()
+
+    def create_qr_code(self):
+        qrcode_img = qrcode.make(
+            config('SERVER_IP') + 
+            reverse('event-detail', args=[str(self.id)])
+        )
+        canvas = Image.new('RGB', (290, 290), 'white')
+        draw = ImageDraw.Draw(canvas)
+        canvas.paste(qrcode_img)
+        fname = f'qrcode-{self.id}.png'
+        buffer = BytesIO()
+        canvas.save(buffer, 'PNG')
+        file = File(buffer, fname)
+        if hasattr(self, 'qrcode'):
+            self.qrcode.image.save(fname, file, save=True)
+        else:
+            QRCode.objects.create(event=self, image=file)
+        canvas.close()
+
     class Meta:
         verbose_name = 'Мероприятие'
         verbose_name_plural = 'Мероприятия'
 
     def __str__(self):
         return self.name
+
+
+class QRCodeDownloadView(UserPassesTestMixin, View):
+    '''Представление для скачивания QR-кода'''
+    def test_func(self):
+        return self.request.user.role in ['admin', 'fed manager', 'reg manager']
+
+    def handle_no_permission(self):
+        return HttpResponse(status=403)
+
+    def get(self, request, *args, **kwargs):
+        event = Event.objects.get(id=kwargs['pk'])
+        img = Image.open(event.qrcode.image)
+        response = HttpResponse(content_type='image/png')
+        img.save(response, 'PNG')
+        return response
 
 
 class Coordinates(models.Model):
@@ -78,7 +140,10 @@ class EventLocation(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     address = models.CharField(max_length=255)
-    coordinates = models.OneToOneField(Coordinates, on_delete=models.CASCADE)
+    coordinates = models.OneToOneField(
+        Coordinates,
+        on_delete=models.CASCADE
+    )
 
     def __str__(self):
         return self.name
