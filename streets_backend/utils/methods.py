51b30639a3781f6b16504c21fa33aa07450b024d
orel3 <orel333@mail.ru>
2024-05-28 21:48:55 +0300

    sender works

--- streets_backend/utils/methods.py
+++ streets_backend/utils/methods.py
@@ -2,5 +2,5 @@ from random import randint
 
 
 def get_confirmation_code():
-    """Получение секретного кода регистрации"""
+    """Получение секретного кода регистрации."""
     return randint(100000000, 999999999)
